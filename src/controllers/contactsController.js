3eb2e4594121ebfb4b6a591cbda2329a200705e7
mycatdoitbetter <andrecasteloecomp@gmail.com>
2020-06-15 23:43:30 -0300

    Finish crud

--- src/controllers/contactsController.js
+++ src/controllers/contactsController.js
@@ -17,6 +17,42 @@ exports.create = async (req, res) => {
   });
 };
 
-exports.read = async (req, res) => {};
+exports.read = async (req, res) => {
+  const response = await db.query("select * from contacts ORDER BY name ASC");
+  res.status(200).send(response.rows);
+};
 
-exports.update = async (req, res) => {};
+exports.update = async (req, res) => {
+  const id = parseInt(req.params.id);
+  const { name, phone, description } = req.body;
+
+  await db.query(
+    "UPDATE contacts SET name = $1, phone = $2, description = $3 WHERE id = $4",
+    [name, phone, description, id]
+  );
+
+  const updatedContact = await db.query(
+    "select * from contacts where id = $1 order by name ASC",
+    [id]
+  );
+
+  res
+    .status(200)
+    .send({ message: "Contact updated!", newContact: updatedContact.rows });
+};
+
+exports.delete = async (req, res) => {
+  const id = parseInt(req.params.id);
+
+  const deletedContact = await db.query(
+    "select * from contacts where id = $1 order by name ASC",
+    [id]
+  );
+
+  await db.query("delete from contacts where id = $1", [id]);
+
+  res.status(200).send({
+    message: "Contact deleted!",
+    delectedContact: deletedContact.rows,
+  });
+};
--- src/routes/contactsRoutes.js
+++ src/routes/contactsRoutes.js
@@ -1,9 +1,9 @@
 const router = require("express-promise-router")();
-const productController = require("../controllers/contactsController");
+const contactsController = require("../controllers/contactsController");
 
-// ==> Definindo as rotas do CRUD - 'Product':
-
-// ==> Rota responsável por criar um novo 'Product': (POST): localhost:3000/api/products
-router.post("/products", productController.create);
+router.post("/contacts", contactsController.create);
+router.get("/contacts", contactsController.read);
+router.put("/contacts/:id", contactsController.update);
+router.delete("/contacts/:id", contactsController.delete);
 
 module.exports = router;
